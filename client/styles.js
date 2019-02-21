c38cfaa60c851d5e7695daa5d7ae1bfafddbbbcf
Thibaut REMY <thibaut@thibaut.re>
2019-02-21 23:51:33 +0100

    chore: add shared styles skeleton

---
+++ client/styles.js
@@ -0,0 +1,16 @@
+export const borderRadius = '8px'
+export const SIZES = {
+  l: '20px',
+  m: '10px',
+  s: '4px',
+}
+
+export const TRANSITIONS = {
+  s: '0.1s',
+  m: '0.2s',
+  l: '0.7s',
+}
+
+export const COLORS = {
+  accent: '#e24f83',
+}
